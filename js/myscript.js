a068ead55ccf704c15cb59e5acf5d17dbbd73a04
martynov-lab <arovitm@gmail.com>
2020-06-04 19:23:06 +0300

    Block Our Services
    1. Adaptive block layout
    2. Number counter when scrolling

--- js/myscript.js
+++ js/myscript.js
@@ -20,3 +20,53 @@ $(window).scroll(function () {
     //сработает когда пользователь доскроллит к элементу с классом .elem
   }
 });
+
+// var number = document.querySelector('.grafic__number'),
+//   numberTop = number.getBoundingClientRect().top,
+//   start = +number.innerHTML, end = +number.dataset.max;
+
+// window.addEventListener('scroll', function onScroll() {
+//   if (window.pageYOffset > numberTop - window.innerHeight / 2) {
+//     this.removeEventListener('scroll', onScroll);
+//     var interval = setInterval(function () {
+//       number.innerHTML = ++start;
+//       if (start == end) {
+//         clearInterval(interval);
+//       }
+//     }, 5);
+//   }
+// });
+
+$(function () {
+  var num = $(".grafic__number");
+  num.each(function (indx, el) {
+    var max = $(el).data("max");
+    var duration = 2000;
+    var visibility = checkViewport(el);
+    $(el).on("animeNum", function () {
+      $({ n: 0 }).animate({ n: max }, {
+        easing: "linear",
+        duration: duration,
+        step: function (now, fx) {
+          $(el).html(now | 0)
+        }
+      })
+    }).data("visibility", visibility);
+    visibility && $(el).trigger("animeNum")
+  });
+
+  function checkViewport(el) {
+    var H = document.documentElement.clientHeight,
+      h = el.scrollHeight,
+      pos = el.getBoundingClientRect();
+    return pos.top + h > 0 && pos.bottom - h < H
+  }
+  $(window).scroll(function () {
+    num.each(function (indx, el) {
+      var visibility = checkViewport(el);
+      el = $(el);
+      var old = el.data("visibility");
+      old != visibility && el.data("visibility", visibility) && !old && el.trigger("animeNum")
+    })
+  })
+});
